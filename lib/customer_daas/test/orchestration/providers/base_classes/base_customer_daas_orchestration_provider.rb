d1868d99c213d1d777289b2e67b9a5a048eccc70
daneb <dane.balia@hetzner.co.za>
2016-04-01 15:08:30 +0200

    Added step defintions for profile creation

--- lib/customer_daas/test/orchestration/providers/base_classes/base_customer_daas_orchestration_provider.rb
+++ lib/customer_daas/test/orchestration/providers/base_classes/base_customer_daas_orchestration_provider.rb
@@ -1,6 +1,26 @@
 module CustomerDaas
   module Test
     class BaseCustomerDaasOrchestrationProvider < BaseOrchestrationProvider
+      def given_a_request_for_customer_creation
+      end
+
+      def  build_a_customer_from_request_params
+      end
+
+      def forward_request_to_relevant_service
+      end
+
+      def customer_profile_has_been_submitted
+      end
+
+      def validate_response_before_returning
+      end
+
+      def return_success_response 
+      end
+
+      def return_fail_response
+      end
     end
   end
 end
