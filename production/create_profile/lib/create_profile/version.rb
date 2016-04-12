2babc7b5a6f51aff67408749df11103835836aaf
daneb <dane.balia@hetzner.co.za>
2016-04-12 11:21:37 +0200

    Added folder structure after losing it

--- production/create_profile/lib/create_profile/version.rb
+++
@@ -1,3 +0,0 @@
-module CreateProfile
-  VERSION = "0.1.0"
-end
---
+++ production/jewels/create_profile/spec/lib/web/models/customer_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe SoarSc::Web::Models::Customer do
+  before :each do
+
+  end
+
+  context 'when initialized' do
+    it 'should remember the customer data transfer object' do
+      pending('TODO')
+    end
+  end
+
+  context 'authenticated when initialized' do
+    pending('TODO')
+  end
+
+  context 'should remember the configuration provided' do
+    pending('TODO')
+  end
+
+  context 'should create the profile' do
+    pending('TODO')
+  end
+
+  context 'should translate/formate the response from profile creation' do
+    pending('TODO')
+  end
+end
